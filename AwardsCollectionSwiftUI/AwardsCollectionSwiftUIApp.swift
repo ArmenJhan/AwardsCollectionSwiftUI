c9637b470715fd845daa7ad9bde20426f071e716
Armen <madarmen1987@gmail.com>
2022-12-26 19:22:37 +0400

    Initial Commit

---
+++ AwardsCollectionSwiftUI/AwardsCollectionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionSwiftUIApp.swift
+//  AwardsCollectionSwiftUI
+//
+//  Created by Armen Madoyan on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
